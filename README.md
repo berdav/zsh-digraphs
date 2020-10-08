268cfa92e2fa97cdcc5f75fa74cf9204f3086323
Davide Berardi <berardi.dav@gmail.com>
2020-10-08 15:20:38 +0200

    First commit with common digraphs

---
+++ README.md
@@ -0,0 +1,64 @@
+# ZSH Digraphs
+
+Insert VIM digraphs with ZSH.
+<!-- gif digraph -->
+
+## Usage
+The plugin uses VIM digraphs.
+The most common ones used for German and Italian are:
+
++------+-------+---------------------------+
+| code | Glyph | Description               |
++------+-------+---------------------------+
+|  a`  |   a   | Grave-accented a (là)     |
++------+-------+---------------------------+
+|  i`  |   i   | Grave-accented i (così)   |
++------+-------+---------------------------+
+|  o`  |   o   | Grave-accented o (però)   |
++------+-------+---------------------------+
+|  e`  |   è   | Grave-accented e (è)      |
++------+-------+---------------------------+
+|  e'  |   é   | Acute-accented e (perché) |
++------+-------+---------------------------+
+|  a:  |   ä   | Umlauted a (männer)       |
++------+-------+---------------------------+
+|  o:  |   ö   | Umlauted o (öl)           |
++------+-------+---------------------------+
+|  u:  |   ü   | Umlauted u (Tschüss)      |
++------+-------+---------------------------+
+|  ss  |   ß   | Eszett (Straße)           |
++------+-------+---------------------------+
+
+To insert one code, write the two characters and press Ctrl-K, the
+characters will get translated to their relative glyph.
+
+## Installation
+To install the plugin put in your `.zshrc.local` the following
+entry:
+```bash
+source $path_to_repo/zsh-digraphs.sh
+```
+substituting $path_to_repo with the path where you've downloaded this repo.
+
+## Update and configuration
+You can generate an up-to-date configuration using the upstream vim
+repository, simply execute the `update_zsh_digraphs.sh` script to generate
+a new `zsh-digraph.sh`.
+
+You can also configure your digraphs, put them in the `user_digraphs.h`
+and regenerate the configuration using the provided
+`update_zsh_digraphs.sh` script.
+
+## Motivation
+From time to time I need to write words on the command line (e.g.
+If I am taking ephemeral notes and I don't want to open VIM).
+
+That is perfectly fine when I'm writing in English, but not when I'm
+writing in Italian (which has accented vowels such as è, é, ù, ò and ì)
+or German which has Ezset (ß) and Umlaut (ü, ö, ä).
+
+The solution could be to use the UTF8 input provided by the System, but
+I would need to remember the conter-intuitive Unicode number.
+
+The VIM input system is really easy to mesmorize.  Therefore this script
+adapt VIM input to ZSH.
---
+++ update-digraphs.sh
@@ -0,0 +1,140 @@
+#!/bin/zsh
+# Update the zsh database of digraphs
+# Copyright (C) 2020  Davide Berardi <berardi.dav@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DIGRAPH_URL="https://raw.githubusercontent.com/vim/vim/master/src/digraph.c"
+TEXEC=/tmp/zsh-digraph
+
+curl "$DIGRAPH_URL" |
+	awk '/digraphdefault\[\] =/,/handle digraphs after/' > "$TEXEC.h"
+echo "*/" >> "$TEXEC.h"
+
+# Generate and execute a C program to generate digraphs associative
+# array.
+cat >"$TEXEC.c" <<_END_
+/**
+ * WARNING WARNING WARNING: This file is auto-generated, please do not
+ * edit but add your digraphs via user_digraphs.h
+ */
+
+/**
+ * Generate the zsh database of digraphs
+ * Copyright (C) 2020  Davide Berardi <berardi.dav@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#define _GNU_SOURCE 1
+#include <stdlib.h>
+#include <locale.h>
+#include <stdio.h>
+#define NUL '\0'
+
+typedef struct {
+	unsigned char code1;
+	unsigned char code2;
+	wchar_t result;
+} digr_T;
+
+#define USE_UNICODE_DIGRAPHS
+#include "$TEXEC.h"
+#include "$(pwd)/user_digraphs.h"
+
+static int need_to_escape(unsigned char c)
+{
+	switch(c) {
+		case '"':
+		case ')':
+		case '(':
+		case '\`':
+		case '\\\\':
+			return 1;
+		default:
+			break;
+	}
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	FILE *out;
+	char *path = NULL;
+	char *home = getenv("HOME");
+	asprintf(&path, "%s/.zsh-digraphs/zsh-digraphs.sh", home);
+	out = fopen(path, "w");
+	free(path);
+
+	if (out == NULL) {
+		perror("fopen");
+		return 1;
+	}
+
+	setlocale(LC_ALL, "");
+	size_t len = sizeof(digraphdefault) / sizeof(*digraphdefault);
+	/* ZSH instructions */
+	fprintf(out, "function digraph {\n");
+	fprintf(out, "\tdeclare -A _digraphs\n");
+	for (int i = 0 ; i < len ; ++i) {
+		if (digraphdefault[i].code1 == 0 || digraphdefault[i].code2 == 0)
+			continue;
+		fprintf(out, "\t_digraphs[\\"%s%c%s%c\\"]=\\"\\\\u%x\\"\n",
+			(need_to_escape(digraphdefault[i].code1)? "\\\\" : ""),
+			digraphdefault[i].code1,
+			(need_to_escape(digraphdefault[i].code2)? "\\\\" : ""),
+			digraphdefault[i].code2,
+			digraphdefault[i].result);
+	}
+	len = sizeof(userdigraphs) / sizeof(*userdigraphs);
+	fprintf(out, "# user digraphs\n");
+	for (int i = 0 ; i < len ; ++i) {
+		if (userdigraphs[i].code1 == 0 || userdigraphs[i].code2 == 0)
+			continue;
+		fprintf(out, "\t_digraphs[\\"%s%c%s%c\\"]=\\"\\\\u%x\\"\n",
+			(need_to_escape(userdigraphs[i].code1)? "\\\\" : ""),
+			userdigraphs[i].code1,
+			(need_to_escape(userdigraphs[i].code2)? "\\\\" : ""),
+			userdigraphs[i].code2,
+			userdigraphs[i].result);
+	}
+	fprintf(out, "\tcode1=\\"\$BUFFER[-2]\\"\n");
+	fprintf(out, "\tcode2=\\"\$BUFFER[-1]\\"\n");
+	fprintf(out, "\tdigraph_to_add=\\"\$(printf \$_digraphs[\\"\$code1\$code2\\"])\\"\n");
+	fprintf(out, "\tLBUFFER=\\"\${LBUFFER[1,-3]}\${digraph_to_add}\\"\n");
+	fprintf(out, "}\n");
+	fprintf(out, "zle -N digraph\n");
+	fprintf(out, "if [[ \$- == *i* ]]; then\n");
+	fprintf(out, "\tbindkey \\"\\\\C-k\\" \\"digraph\\"\n");
+	fprintf(out, "fi\n");
+
+	fclose(out);
+	return 0;
+}
+_END_
+
+make "$TEXEC"
+"$TEXEC"
+
+rm -rf $TEXEC{,.c,.h}
